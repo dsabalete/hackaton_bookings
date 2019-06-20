4cfc5650ca9013ebb87545558057a13df587d691
David Sabalete <david.sabalete@odigeo.org>
2019-06-20 10:07:31 +0200

    button deleted

--- stories/buttons.stories.js
+++ stories/buttons.stories.js
@@ -3,27 +3,10 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { jsxDecorator } from 'storybook-addon-jsx';
 
-import OdigeoDefaultButton from '../components/OdigeoButtons/OdigeoDefaultButton'
-import OdigeoDisabledButton from '../components/OdigeoButtons/OdigeoDisabledButton'
-import OdigeoSelectedButton from '../components/OdigeoButtons/OdigeoSelectedButton'
-import OdigeoFocusErrorButton from '../components/OdigeoButtons/OdigeoFocusErrorButton'
-
 import Primary from '../components/OdigeoButtons/Primary'
 
 storiesOf('Primary Odigeo Buttons', module)
   .addDecorator(jsxDecorator)
-  .add('Default Button', () => (
-    <OdigeoDefaultButton>Book now</OdigeoDefaultButton>
-  ))
-  .add('Disabled Button', () => (
-    <OdigeoDisabledButton>Book now</OdigeoDisabledButton>
-  ))
-  .add('Selected Button', () => (
-    <OdigeoSelectedButton>Book now</OdigeoSelectedButton>
-  ))
-  .add('Focus-error Button', () => (
-    <OdigeoFocusErrorButton>Book now</OdigeoFocusErrorButton>
-  ))
   .add('Primary', () => (
     <Primary>Book now</Primary>
   ))
