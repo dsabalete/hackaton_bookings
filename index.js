01f121024b987b73ce270df5bc6225d977c3d82f
David Sabalete <david.sabalete@odigeo.org>
2019-06-20 12:31:20 +0200

    Update exports in index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import Primary from './components/OdigeoButtons/Primary'
 import Input from './components/Forms/Input'
 import Hamburguer from './components/Hamburguer/Hamburguer'
+import Popup from './components/Modals/Popup'
 
 export {
     Primary,
     Input,
     Hamburguer,
+    Popup
 }
