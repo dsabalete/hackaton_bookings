e6a880ea0b160020ca65f4758d60df8af206c7ae
oscar.bellon <oscar.bellon@odigeo.com>
2019-06-20 12:21:29 +0200

    Add Hamburguer component

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import Primary from './components/OdigeoButtons/Primary'
 import Input from './components/Forms/Input'
+import Hamburguer from './components/Hamburguer/Hamburguer'
 
 export {
     Primary,
     Input,
+    Hamburguer,
 }
