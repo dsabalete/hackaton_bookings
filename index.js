d9881456a58173754c42221ee99cb0b073c19fb2
oscar.bellon <oscar.bellon@odigeo.com>
2019-06-20 11:01:19 +0200

    Add Input component

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-import OdigeoDefaultButton from './components/OdigeoButtons/OdigeoDefaultButton'
-import Input from './components/Forms/Input'
-
-
-export const OdigeoDefaultButton;
-export const Input;
+export {Primary} from './components/OdigeoButtons/Primary';
+export {Input} from './components/Forms/Input';
